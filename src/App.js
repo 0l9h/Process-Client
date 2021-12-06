e95881a01e1edc3f60d865fd769401c45a4ea512
N0Test <82705740+N0Test@users.noreply.github.com>
2021-12-06 10:32:34 +0200

    Table generation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-
 import logo from "./logo.svg";
 import "./App.css";
 import InputForm from "./components/Input/input.jsx";
 import React from "react";
-import Table from './components/Table/Table.jsx'
-import $ from "jquery";
-
+import ReactDOM from 'react-dom';
+import Table from "./components/Table/Table.jsx";
+import react from "react";
 
 class App extends React.Component {
   state = {};
 
   handleClick = async (userInput) => {
-
-    userInput.dir.replace("\\",/\\/);
-    const response = await fetch("http://localhost:30629/api/FilesInfo",
-        {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body:JSON.stringify({
-            Path: userInput.dir,
-            Extension: userInput.extension,
-            })
-        }).then((resp) => resp.json())
-        console.log(response);
-  }
+    userInput.dir.replace("\\", /\\/);
+    const response = await fetch("http://localhost:30629/api/FilesInfo", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        Path: userInput.dir,
+        Extension: userInput.extension,
+      }),
+    }).then((resp) => resp.json());
+    ReactDOM.render(
+      <Table files={response.files} totalSize={response.totalSize} />,
+      document.getElementById("table")
+    );
+  };
 
   render() {
     return (
       <div className="container">
-      <div>
-        <InputForm onClick={this.handleClick} />
-      </div>
-      <div id="table" className="col-8 mx-auto"></div>
+        <div>
+          <InputForm onClick={this.handleClick} />
+        </div>
+        <div id="table" className="col-8 mx-auto"></div>
       </div>
     );
   }
